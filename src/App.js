085103bce420d34ced62c4704966535746abcab6
sanjaisharan <harisanjay1967@gmail.com>
2023-11-18 10:18:55 +0530

    day4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import FruitSurvey from "./componets/day4/FruitSurvey";
+import MaterialButton from "./componets/day4/MaterialButton";
 
 
 export default function app(){
   return(
   
-<FruitSurvey></FruitSurvey>
+<MaterialButton></MaterialButton>
     
    
   );
